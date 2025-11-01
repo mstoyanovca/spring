190e87924d6e84a630bef2585b61ea1dac35c0c6
mstoyanovca <mstoyanovca@gmail.com>
2025-11-01 16:43:22 -0400

    created module reactive

---
+++ spring/reactive/src/main/java/reactive/ReactiveApplication.java
@@ -0,0 +1,13 @@
+package reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class ReactiveApplication {
+    static void main(String[] args) {
+        SpringApplication.run(ReactiveApplication.class, args);
+    }
+}
---
+++ spring/reactive/src/test/java/reactive/ReactiveApplicationTests.java
@@ -0,0 +1,11 @@
+package reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
