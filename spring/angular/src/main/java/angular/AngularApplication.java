06e3d5c4cb8389a8740931460718053cd78cead8
mstoyanovca <mstoyanovca@gmail.com>
2025-11-09 10:25:08 -0500

    created project angular

---
+++ spring/angular/src/main/java/angular/AngularApplication.java
@@ -0,0 +1,11 @@
+package angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularApplication {
+    static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(AngularApplication.class, args);
+    }
+}
---
+++ spring/angular/src/test/java/angular/AngularApplicationTests.java
@@ -0,0 +1,11 @@
+package angular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
