6e90a73907d3203f453b6a6ba386dba544d39e90
mstoyanovca <mstoyanovca@gmail.com>
2025-11-09 21:19:27 -0500

    working

--- spring/login/front-end/src/app/app.routes.ts
+++ spring/login/front-end/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './login-page/login-page';
 import { Customer } from './customer/customer';
 
 export const routes: Routes = [
-    {
-        path: 'customer',
-        component: Customer,
-    },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginPage },
+    { path: 'customer', component: Customer }
 ];
